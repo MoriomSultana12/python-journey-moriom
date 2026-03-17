af091ab04c48f9249c1f30d81f49068e96489d3c
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-17 11:29:38 +0600

    Demonstrate variable reassignment in Python

---
+++ 01_basics/variables.py
@@ -0,0 +1,6 @@
+#assign value 10 to variable " price "
+price = 10 # initial value
+#reassign the value 20 to the same variable (overwrites previous value)
+price = 20 # updated value
+#print the current value of price
+print(price)  # outputs the latest value (20)
