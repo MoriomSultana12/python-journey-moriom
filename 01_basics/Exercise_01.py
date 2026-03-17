f9174a3f756c07bd845e80885fe2a663c0d1a2fe
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-17 12:10:37 +0600

    Add patient information example using variables

---
+++ 01_basics/Exercise_01.py
@@ -0,0 +1,12 @@
+# Hospital problem :
+# There is a man named Smith ; He is 35 years old.
+# Need to check if he is an existing patient or new patient.
+
+Name = 'Smith'                 # String - patient's name
+age = 35                       # Integer - patient's age
+is_new = True                  # Boolean - True means new patient ; False means existing patient
+
+
+print(Name)
+print(age)
+print(is_new)
