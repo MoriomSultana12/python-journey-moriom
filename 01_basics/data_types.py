4faa485d14c52192eb54c2c4cce96ce10e567d8d
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-17 11:56:55 +0600

    Demonstrate variables and data types in Python

---
+++ 01_basics/data_types.py
@@ -0,0 +1,15 @@
+# Assigning values to variables using different data types
+
+print_1 = 10                 #Integer(int) - whole number
+Print_2 = 20.89              #Float (float) - decimal number
+Name = "Moriom"              #String (str) - text must be inside the quotes
+is_published = False         #Boolean (bool) - True or False (must start with Capital letter)
+
+# Printing the values of variables
+
+print(Print_2)                 #output 20.89
+print(print_1)                 #output 10
+print(is_published)            #output False
+print(Name)                    #output Moriom
+
+
