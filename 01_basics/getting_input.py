2983b2e7d38be5d0b225360b5f37e845597f1928
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-17 12:28:11 +0600

    Add user input example with string concatenation

---
+++ 01_basics/getting_input.py
@@ -0,0 +1,8 @@
+# Taking user input from keyboard
+
+name = input(" What's your name? ")        # input() takes text input from user and stores it in 'name'
+
+# Printing a message using the input
+print("Hi " + name + "! Yesterday sir took a very important class.")
+
+# Combines (concatenates) strings with the variable 'name'
