7c8b8ce6306d760933c76f6679ab585ad1e57b03
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-19 12:10:16 +0600

    multi-line string example using triple quotes

---
+++ 01_basics/11_Multiple_line.py
@@ -0,0 +1,8 @@
+# Using triple quotes (''' ''') to write a multi-line string
+
+course = '''
+Consistency drives success. 
+Embrace your challenges and keep showing up, 
+even when it feels uncomfortable.
+'''
+print (course)
