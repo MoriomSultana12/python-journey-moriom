f9979ee6b6b24574310469d99683cb016ab5fa49
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-19 12:02:45 +0600

    string examples demonstrating use of single and double quotes

---
+++ 01_basics/10_string.py
@@ -0,0 +1,9 @@
+
+# Using double quotes so we can include an apostrophe (') inside the string
+course= "Python's course for beginners "
+print(course)
+
+# Using single quotes so we can include double quotes (") inside the string
+Course_1= 'Python course for "beginners"'
+print(Course_1)
+
