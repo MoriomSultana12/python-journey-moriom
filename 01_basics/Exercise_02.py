5d65c1ced847dc4caa8f32a3d84a8b06f396c225
FirstProject-demo <moriomsultana2000@gmail.com>
2026-03-19 11:14:37 +0600

    Implement basic input and string concatenation example

---
+++ 01_basics/Exercise_02.py
@@ -0,0 +1,11 @@
+#Write a Python program that:
+#Takes the user’s name as input
+#Takes the user’s favorite color as input
+#Prints a sentence Moriom likes pink
+
+# Taking user's name as input and storing it in a variable called 'name'
+name = input('Whats your name ?')
+# Taking user's favorite color as input and storing it in a variable called 'color'
+color = input ("whats your fav color?")
+# Printing the result by combining (concatenating) name and color
+print(name+'likes'+color)
